b6c7c97567126b9302abc197668e13b616d52ca3
espy02 <82273962+espy02@users.noreply.github.com>
2022-12-27 12:50:17 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -28,42 +28,42 @@ def minesCoordinates(rows, columns, mines):
 
 def cellsCoordinates(rows, columns):
     '''
-    This function will create a dictionary that will contain every coordinate of each cell.
+    This function will create a dictionary that will contain the coordinates of each cell.
     The keys of the dictionary are the number of each row, which were assigned in start.py.
     The values are a list containing the "?" symbol.
     The number of "?"s is equivalent to the number of columns assigned in start.py.
     Since the lists' index number 0 won't be used, it will be assigned with "N/A".
     This function will return said dictionary with the cells' coordinates.
     '''
-    rowsNcolumns = {}
+    cells = {}
 
     for i in range(1, rows + 1):
-        rowsNcolumns[i] = []
+        cells[i] = []
         
         for j in range(columns + 1):
             if j != 0:
-                rowsNcolumns[i].append("?")
+                cells[i].append("?")
             else:
-                rowsNcolumns[i].append("N/A")
+                cells[i].append("N/A")
 
-    return rowsNcolumns
+    return cells
 
-def showCells(points, totalPoints, flags, moves, rowsNcolumns, columns):
+def showCells(points, totalPoints, flags, moves, cells, columns):
     '''
-    This function will print the rowsNcolumns dictionary, each element in one line.
+    This function will print the cells dictionary, each element in one line.
 
     First, it will print the numbers at the top, the number of each column.
-    If there are more than 10 columns, if the column number is lesser or equal than 9, it will also print two blank spaces next to it.
-    If there are more than 10 columns, if the column number is greater than 9, it will also print one blank space next to it.
-    If there are 10 columns or less, it will always print one blank space next to each number.
+    If there are more than 10 columns, if the column number is lesser or equal than 9, print two blank spaces next to the number.
+    If there are more than 10 columns, if the column number is greater than 9, print one blank space next to the number.
+    If there are 10 columns or less, print one blank space next to each number.
 
     Next, it will print the numbers on the left, the number of each row.
-    If the row number is lesser or equal than 9, it will also print three blank spaces next to it.
-    If the row number is greater than 9, it will also print two blank spaces next to it.
+    If the row number is lesser or equal than 9, print three blank spaces next to the number.
+    If the row number is greater than 9, print two blank spaces next to the number.
 
     Finally, it will print the cells of each row.
-    If there are more than 10 columns, it will also print two blank spaces next to each number.
-    If there are 10 columns or less, it will also print one blank space next to each number.
+    If there are more than 10 columns, print two blank spaces next to each number.
+    If there are 10 columns or less, print one blank space next to each number.
     '''
     print(f"\n\nPoints: {points} / {totalPoints}\nFlags: {flags}\nMoves: {moves}\n")
 
@@ -78,7 +78,7 @@ def showCells(points, totalPoints, flags, moves, rowsNcolumns, columns):
             print(i, end = " ")
     print("\n")
 
-    for i in rowsNcolumns.keys():
+    for i in cells.keys():
         if i > 9:
             print(i, end = "  ")
         else:
@@ -86,63 +86,19 @@ def showCells(points, totalPoints, flags, moves, rowsNcolumns, columns):
 
         for j in range(1, columns + 1):
             if columns > 10:
-                print(rowsNcolumns[i][j], end = "  ")
+                print(cells[i][j], end = "  ")
             else:
-                print(rowsNcolumns[i][j], end = " ")
+                print(cells[i][j], end = " ")
 
         print("")
     print("")
 
-def checkMinesAround(rowLeft, rowRight, columnLeft, columnRight, minesCells, minesAround):
-    '''
-    This 
-    '''
-    for i in range(rowLeft, rowRight):
-        for j in range(columnLeft, columnRight):
-            if [i, j] in minesCells:
-                minesAround += 1
-                
-    return minesAround
-
-def showMines(minesCells, rows):
-    '''
-    
-    '''
-    for i in minesCells:
-        rows[i[0]][i[1]] = "M"
-
-def checkMinesAroundPreamble(rowChosen, columnChosen, rows, columns, minesCells):
-    '''
-    
-    '''
-    minesAround = 0
-
-    if rowChosen != 1 and columnChosen != 1 and rowChosen != rows and columnChosen != columns:
-        minesAround = checkMinesAround(rowChosen - 1, rowChosen + 2, columnChosen - 1, columnChosen + 2, minesCells, minesAround)
-                    
-    elif rowChosen == 1 and columnChosen != 1:
-        minesAround = checkMinesAround(rowChosen, rowChosen + 2, columnChosen - 1, columnChosen + 2, minesCells, minesAround)
-
-    elif rowChosen != 1 and columnChosen == 1:
-        minesAround = checkMinesAround(rowChosen - 1, rowChosen + 2, columnChosen, columnChosen + 2, minesCells, minesAround)
-
-    elif rowChosen == 1 and columnChosen == 1:
-        minesAround = checkMinesAround(rowChosen, rowChosen + 2, columnChosen, columnChosen + 2, minesCells, minesAround)
-
-    elif rowChosen == rows and columnChosen != columns:
-        minesAround = checkMinesAround(rowChosen - 1, rowChosen + 1, columnChosen - 1, columnChosen + 2, minesCells, minesAround)
-
-    elif rowChosen != rows and columnChosen == columns:
-        minesAround = checkMinesAround(rowChosen - 1, rowChosen + 2, columnChosen - 1, columnChosen + 1, minesCells, minesAround)
-
-    elif rowChosen == rows and columnChosen == columns:
-        minesAround = checkMinesAround(rowChosen - 1, rowChosen + 1, columnChosen - 1, columnChosen + 1, minesCells, minesAround)
-
-    return minesAround
-
 def checkMoves(points, flags, moves, lastPlay):
     '''
-    
+    This function will check if the user has made a move.
+    A user has made a move if the number of points or the number of flags has changed since the last play.
+    The lastPlay list contains the number of points and the number of flags of the last play.
+    This function will return the number of moves.
     '''
     if lastPlay[0] != points or lastPlay[1] != flags:
         lastPlay.extend([points, flags])
@@ -151,3 +107,80 @@ def checkMoves(points, flags, moves, lastPlay):
         return moves + 1
     else:
         return moves
+
+def showMines(cellsWithMines, cells):
+    '''
+    This function will change the cells in the cells dictionary that contain a mine to the character "M".
+    This function is called when the user loses the game or wins the game.
+    '''
+    for i in cellsWithMines:
+        cells[i[0]][i[1]] = "M"
+
+def checkMinesAround(rowChosen, columnChosen, rows, columns, cellsWithMines):
+    '''
+    
+    '''
+    minesAround = 0
+    rowLeft = rowChosen - 1
+    rowRight = rowChosen + 2
+    columnLeft = columnChosen - 1
+    columnRight = columnChosen + 2
+      
+    if rowChosen == 1 and columnChosen != 1:
+        rowLeft = rowChosen
+
+    elif rowChosen != 1 and columnChosen == 1:
+        columnLeft = columnChosen
+
+    elif rowChosen == 1 and columnChosen == 1:
+        rowLeft = rowChosen
+        columnLeft = columnChosen
+
+    elif rowChosen == rows and columnChosen != columns:
+        rowRight = rowChosen + 1
+
+    elif rowChosen != rows and columnChosen == columns:
+        columnRight = columnChosen + 1
+
+    elif rowChosen == rows and columnChosen == columns:
+        rowRight = rowChosen + 1
+        columnRight = columnChosen + 1
+
+    for i in range(rowLeft, rowRight):
+        for j in range(columnLeft, columnRight):
+            if [i, j] in cellsWithMines:
+                minesAround += 1
+
+    return minesAround
+
+def checkCellsAround(rowChosen, columnChosen, rows, columns, cellsSelected, cellsWithMines, cells, selectedCellMinesAround, points):
+    '''
+    This function will check if the cell above, below, to the right, and to the left of the selected cell has the same number of mines around it.
+    Those cells will be added to cellsAround.
+    The cells that have a 0 on its coordinates, or have a coordinate that is greater than the respective number of rows or number of columns,
+    will be removed from the list.
+    The cells that have the same number of mines around as the selected cell will be automatically selected and added to cellsSelected.
+    Then, just like the original selected cell, the cell above, below, to the right, and to the left of a cell added to cellsSelected,
+    will also be added to cellsAround.
+    Once a cell is verified, it will be removed from cellsAround*.
+    If the cell is in cellsSelected, or is a cell with a mine, or is a cell with a flag, or has a 0 on its coordinates,
+    or has a coordinate that is greater than the respective number of rows or number of columns, 
+    then it will not be verified and it will be removed from cellsAround.
+    *(except the original selected cell, since it was never in the list to begin with)
+    '''
+    cellsAround = [[rowChosen - 1, columnChosen], [rowChosen, columnChosen - 1], [rowChosen, columnChosen + 1], \
+    [rowChosen + 1, columnChosen]]
+
+    while len(cellsAround) != 0:
+        if cellsAround[0][0] > 0 and cellsAround[0][0] <= rows and cellsAround[0][1] > 0 and cellsAround[0][1] <= columns \
+        and [cellsAround[0][0], cellsAround[0][1]] not in cellsSelected and [cellsAround[0][0], cellsAround[0][1]] not in cellsWithMines \
+        and cells[cellsAround[0][0]][cellsAround[0][1] - 1] != "F" \
+        and checkMinesAround(cellsAround[0][0], cellsAround[0][1], rows, columns, cellsWithMines) == selectedCellMinesAround:
+            cells[cellsAround[0][0]][cellsAround[0][1]] = selectedCellMinesAround
+            cellsSelected.append([cellsAround[0][0], cellsAround[0][1]])
+            points += 1
+            cellsAround.extend([[cellsAround[0][0] - 1, cellsAround[0][1]],  [cellsAround[0][0], cellsAround[0][1] - 1], \
+            [cellsAround[0][0], cellsAround[0][1] + 1], [cellsAround[0][0] + 1, cellsAround[0][1]]])
+        cellsAround.pop(0)
+
+    return points
