91d9273982f712ae824f7769b51269d4a69bbaed
espy02 <82273962+espy02@users.noreply.github.com>
2022-12-27 12:50:20 +0000

    Update game.py

--- game.py
+++ game.py
@@ -2,94 +2,72 @@ from functions import *
 
 def minesweeper(rows, columns, mines):
     cellsWithMines = minesCoordinates(rows, columns, mines)
-
-    rowsNcolumns = cellsCoordinates(rows, columns)
+    
+    cells = cellsCoordinates(rows, columns)
 
     points = 0
     totalPoints = (rows * columns) - mines
     flags = mines
     moves = 0
+
     cellsSelected = []
     lastPlay = [points, flags]
     playing = True
 
     while playing:
-        showCells(points, totalPoints, flags, moves, rowsNcolumns, columns)
+        showCells(points, totalPoints, flags, moves, cells, columns)
         print("1. Cell\n2. Flag\n")
         choice = input("Select (1/2): ")
 
         match choice:
             case "1":
+                cellChosen = input("Select cell (row, column): ")
                 try:
-                    rowChosen = int(input("Row: "))
-                    columnChosen = int(input("Column: "))
-                except ValueError:
+                    rowChosen = int(cellChosen.split(",")[0].strip())
+                    columnChosen = int(cellChosen.split(",")[1].strip())
+                except (ValueError, IndexError):
                     rowChosen = 0
                     columnChosen = 0
 
-                if [rowChosen, columnChosen] in cellsWithMines and rowsNcolumns[rowChosen][columnChosen] != "F":
+                if [rowChosen, columnChosen] in cellsWithMines and cells[rowChosen][columnChosen] != "F":
                     playing = False
-                    showMines(cellsWithMines, rowsNcolumns)
                     moves = checkMoves(points, flags, moves, lastPlay)
-                    showCells(points, totalPoints, flags, moves, rowsNcolumns, columns)
+                    showMines(cellsWithMines, cells)
+                    showCells(points, totalPoints, flags, moves, cells, columns)
                     print("You lost!")
 
                 elif rowChosen > 0 and rowChosen <= rows and columnChosen > 0 and columnChosen <= columns \
-                and rowsNcolumns[rowChosen][columnChosen] != "F" and [rowChosen, columnChosen] not in cellsSelected:
-                    minesAround = checkMinesAroundPreamble(rowChosen, columnChosen, rows, columns, cellsWithMines)
-                    rowsNcolumns[rowChosen][columnChosen] = minesAround
-                    playedCellMinesAround = minesAround
+                and cells[rowChosen][columnChosen] != "F" and [rowChosen, columnChosen] not in cellsSelected:
+
+                    selectedCellMinesAround = checkMinesAround(rowChosen, columnChosen, rows, columns, cellsWithMines)
+                    cells[rowChosen][columnChosen] = selectedCellMinesAround
                     cellsSelected.append([rowChosen, columnChosen])
                     points += 1
 
-                    cellsAround = [[rowChosen - 1, columnChosen], [rowChosen, columnChosen - 1], [rowChosen, columnChosen + 1], \
-                    [rowChosen + 1, columnChosen]]
-
-                    for i in range(3):
-                        checkCellsAround = 0
-                        while checkCellsAround != 3:
-                            if checkCellsAround < len(cellsAround):
-                                if cellsAround[checkCellsAround][0] <= 0 or cellsAround[checkCellsAround][0] > rows \
-                                or cellsAround[checkCellsAround][1] <= 0 or cellsAround[checkCellsAround][1] > columns:
-                                    cellsAround.remove([cellsAround[checkCellsAround][0], cellsAround[checkCellsAround][1]])
-                            checkCellsAround += 1
-
-                    while len(cellsAround) != 0:
-                        if cellsAround[0][0] > 0 and cellsAround[0][0] <= rows \
-                        and cellsAround[0][1] > 0 and cellsAround[0][1] <= columns \
-                        and [cellsAround[0][0], cellsAround[0][1]] not in cellsSelected \
-                        and [cellsAround[0][0], cellsAround[0][1]] not in cellsWithMines \
-                        and rowsNcolumns[cellsAround[0][0]][cellsAround[0][1] - 1] != "F":
-                            if checkMinesAroundPreamble(cellsAround[0][0], cellsAround[0][1], rows, columns, cellsWithMines) \
-                            == playedCellMinesAround:
-                                rowsNcolumns[cellsAround[0][0]][cellsAround[0][1]] = playedCellMinesAround
-                                cellsSelected.append([cellsAround[0][0], cellsAround[0][1]])
-                                points += 1
-                                cellsAround.extend([[cellsAround[0][0] - 1, cellsAround[0][1]],  [cellsAround[0][0], cellsAround[0][1] - 1], \
-                                [cellsAround[0][0], cellsAround[0][1] + 1], [cellsAround[0][0] + 1, cellsAround[0][1]]])
-                        cellsAround.pop(0)
+                    points = checkCellsAround(rowChosen, columnChosen, rows, columns, cellsSelected, cellsWithMines, cells, selectedCellMinesAround, points)
 
                 if points == totalPoints:
                     playing = False
-                    showMines(cellsWithMines, rowsNcolumns)
                     moves = checkMoves(points, flags, moves, lastPlay)
-                    showCells(points, totalPoints, flags, moves, rowsNcolumns, columns)
+                    showMines(cellsWithMines, cells)
+                    showCells(points, totalPoints, flags, moves, cells, columns)
                     print("You win!")
 
             case "2":
+                cellChosen = input("Select cell (row, column): ")
                 try:
-                    rowChosen = int(input("Row: "))
-                    columnChosen = int(input("Column: "))
-                except ValueError:
+                    rowChosen = int(cellChosen.split(",")[0].strip())
+                    columnChosen = int(cellChosen.split(",")[1].strip())
+                except (ValueError, IndexError):
                     rowChosen = 0
                     columnChosen = 0
 
                 if rowChosen > 0 and rowChosen <= rows and columnChosen > 0 and columnChosen <= columns:
-                    if rowsNcolumns[rowChosen][columnChosen] == "?" and flags > 0:
-                        rowsNcolumns[rowChosen - 1][columnChosen] = "F"
+                    if cells[rowChosen][columnChosen] == "?" and flags > 0:
+                        cells[rowChosen][columnChosen] = "F"
                         flags -= 1
-                    elif rowsNcolumns[rowChosen][columnChosen] == "F" and flags < mines:
-                        rowsNcolumns[rowChosen][columnChosen] = "?"
+                    elif cells[rowChosen][columnChosen] == "F" and flags < mines:
+                        cells[rowChosen][columnChosen] = "?"
                         flags += 1
 
             case _:
