3619c6c6ba191486811bdd340bb21653ac2ff3d0
espy02 <82273962+espy02@users.noreply.github.com>
2022-12-28 20:49:49 +0000

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 from game import minesweeper
+from getpass import getpass
 
 playing = True
 
@@ -11,15 +12,15 @@ while playing:
 
     match difficulty:
         case "1":
-            input("\nNote: to select a cell, enter the number of the row, and the number of the column, separated by a comma.\nExample: 4,5\nPress enter to continue.")
+            getpass("\nNote: to select a cell, enter the number of the row, and the number of the column, separated by a comma.\nExample: 4,5\nPress enter to continue.")
             minesweeper(rows = 10, columns = 10, mines = 10)
 
         case "2":
-            input("\nNote: to select a cell, enter the number of the row, and the number of the column, separated by a comma.\nExample: 4,5\nPress enter to continue.")
+            getpass("\nNote: to select a cell, enter the number of the row, and the number of the column, separated by a comma.\nExample: 4,5\nPress enter to continue.")
             minesweeper(rows = 16, columns = 16, mines = 40)
 
         case "3":
-            input("\nNote: to select a cell, enter the number of the row, and the number of the column, separated by a comma.\nExample: 4,5\nPress enter to continue.")
+            getpass("\nNote: to select a cell, enter the number of the row, and the number of the column, separated by a comma.\nExample: 4,5\nPress enter to continue.")
             minesweeper(rows = 30, columns = 16, mines = 99)
 
         case "4":
